74fc9e4b00bbed1721ba677fa2023b4256963a17
ariviegea <ariadna.villar.egea@gmail.com>
2019-12-05 16:34:01 +0100

    server ready for mongodb implementation

--- server.js
+++ server.js
@@ -1,5 +1,28 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const port = 5000;
+
+const apartments = [
+    {
+        title: 'Consell de cent',
+        description: 'BLABLABLA',
+        available: true,
+    },
+    {
+        title: 'Paral.lel 21',
+        description: 'BLABLABLA',
+        available: true,
+    }
+]
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// GET
+app.get('/api/apartments', (req, res) => {
+    res.status(200).json(apartments);
+});
 
 app.listen(port, () => console.log(`Server running in port ${port}`));
