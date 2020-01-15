f9f35a7de0b30f57d19cf7322739b80ae0776da2
ariviegea <ariadna.villar.egea@gmail.com>
2020-01-15 14:42:03 +0100

    updated actions file

--- backend/src/mongodb/schema.js
+++ backend/src/mongodb/schema.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const UserSchema = new mongoose.Schema({
+const AdminSchema = new mongoose.Schema({
     email: {
         type: String,
         required: true,
