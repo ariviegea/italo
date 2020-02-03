91ec5f6b77980e815928030e0540a985a8b9f6a3
ariviegea <ariadna.villar.egea@gmail.com>
2020-02-03 15:59:32 +0100

    Created userlogin authentication server & rearranged admin client

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,37 +1,86 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const mongoClient = require('./mongodb/connect');
-// const userAdmin = require('./mongodb/schema');
+const uuidv4 = require('uuid/v4');
+const bcrypt = require('bcrypt');
 
-mongoClient.setupDb();
 
-const apartments = [
+const adminAccess = [
     {
-        title: 'Consell de cent',
-        description: 'BLABLABLA',
-        available: true,
+        id: uuidv4(),
+        name: 'ricard',
+        password: 'ricard123'
     },
     {
-        title: 'Paral.lel 21',
-        description: 'BLABLABLA',
-        available: true,
-    }
+        id: uuidv4(),
+        name: 'ariadna',
+        password: 'ariadna123'
+    },
 ]
 
 // Body parser middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(express.json());
 
-// GET
-app.get('/api/apartments', (req, res) => {
-    res.status(200).json(apartments);
+// Error middleware
+app.use((err, req, res, next) => {
+    res.status(500).send('Something broke!');
+    next(err);
+  });
+  
+app.use((err, req, res, next) => {
+    res.status(404).send('Error 404 occurred');
+    next(err);
+  });
+
+// Get all users
+app.get('/admin', (req, res)=> {
+    res.status(200).json(adminAccess);
 });
 
-// POST adminUser 
-app.post('/admin', async(res, req) => {
- const { email, password } = req.body;
+// Get one user 
+app.get('/admin/:id', (req, res) => {
+    let user = adminAccess.find(user => user.id === req.params.id);
+    if( user ){
+        res.status(200).json(user);
+    } else {
+        res.status(400).json({msg: 'Please fill a real user'});
+    }
+})
+
+// Send user 
+// Create a user admin 
+app.post('/admin', async(req, res) => {
+    try {
+        const hashPassword = await bcrypt.hash(req.body.password, 10);
+        let user = { 
+            id: uuidv4(), 
+            name: req.body.name, 
+            password: hashPassword 
+        }
+        adminAccess.push(user);
+        res.status(201).send()
+    } catch {
+        res.status(500);
+    } 
 });
+
+// Login the admin
+app.post('/admin/login', async(req, res) => {
+    const user = adminAccess.find(user => user.name === req.body.name);
+    if ( user == null) {
+        return res.status(400).send('The user can not be found');
+    } try {
+        if (await bcrypt.compare(req.body.password, user.password)) {
+            res.send('Success');
+        } else {
+            res.send('Access not allowed');
+        }
+    } catch {
+        res.status(500).send();
+    }
+})
 
 module.exports = {
     app,
--- client/src/components/Admin/Admin.jsx
+++ client/src/components/Admin/Admin.jsx
@@ -5,20 +5,18 @@ const Admin = () => {
   return (
     <div className='admin'>
       <h1>Administration</h1>
-      <div className='admin__access'>
-        <ul>
-          <li>
-            <p>Name</p>
-            <input></input>
-          </li>
-          <li>
-            <p>Password</p>
-            <input></input>
-          </li>
-          <li>
-            <button>Submit</button>
-          </li>
-        </ul>
+      <div className='admin__login'>
+          <div className='admin__login__input'>
+            <label>Name</label>
+            <input type='text' name='name' placeholder='Name' size='20' required></input>
+          </div>
+          <div className='admin__login__input'>
+            <label>Password</label>
+            <input type='text' name='password' placeholder='Password' size='20' required></input>
+          </div>
+          <div className='admin__login__button'>
+            <button type='submit'>Sign up</button>
+          </div>
       </div>
     </div>
   )
